e998be4bab36aeade17581156a38a736489764ba
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-12-30 00:13:35 +0100

    ..

--- src/components/Pages/HomePage/HomePage.jsx
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -10,24 +10,24 @@ function HomePage() {
   return (
     <div className={Styles.homePage}>
       <h2 className="font-bold">Welcome to</h2>
-      <h1 className="font-bold text-2xl mb-4">MayariChat</h1>
+      <h1 className="font-bold text-4xl mb-20">MayariChat</h1>
 
-      <Card className=" text-white bg-gray-800 w-[26rem]  ">
+      <Card className=" text-white bg-gray-800 w-[26rem] ">
         <CardHeader className="p-4 ">
           <h1 className="font-bold text-xl ">Login:</h1>
         </CardHeader>
         <Divider className="bg-gray-700" />
-        <CardBody className="p-4">
+        <CardBody className="p-4 text-gray-200">
           <p>Only authorized user can login to this app. please login here and enjoy.</p>
         </CardBody>
         <Divider className="bg-gray-700" />
-        <CardFooter className="p-4">
+        <CardFooter className="py-4 ">
           <Button
             onClick={() => {
               navigate("../chat");
             }}
-            className="w-full text-md font-bold "
-            color="primary"
+            className="w-full text-md font-bold rounded-small"
+            color="secondary"
             variant="solid"
             endContent={<ArrowForwardIosRoundedIcon />}
           >
