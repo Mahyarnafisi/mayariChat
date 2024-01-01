bb9dd5672dcffaabcb6c1459182f76dbb48ac615
mahyarnafisi <mahyar.nafisi@gmail.com>
2024-01-01 12:00:23 +0100

    implement header user live panel

--- src/components/Pages/ChatPage/ChatPanel/ChatPanel.jsx
+++ src/components/Pages/ChatPage/ChatPanel/ChatPanel.jsx
@@ -1,13 +1,15 @@
-import { Card, CardBody, CardFooter, CardHeader, Divider } from "@nextui-org/react";
 import React from "react";
+import { Card, CardBody, CardFooter, CardHeader, Divider } from "@nextui-org/react";
 import ChatPanelFooter from "./ChatPanelFooter/ChatPanelFooter";
+import Styles from "./ChatPanel.module.css";
+import LiveUsers from "./ChatPanelHeader/LiveUsers/LiveUsers";
 
 function ChatPanel() {
   return (
-    <div>
+    <div className={Styles.chatPanel}>
       <Card className="m-8 h-96 ">
-        <CardHeader>
-          <h1>chat page</h1>
+        <CardHeader className="p-4">
+          <LiveUsers />
         </CardHeader>
         <Divider />
         <CardBody className="bg-slate-300 h-full">
---
+++ src/components/Pages/ChatPage/ChatPanel/ChatPanelHeader/LiveUsers/LiveUsers.jsx
@@ -0,0 +1,23 @@
+import { User } from "@nextui-org/react";
+import React from "react";
+
+function LiveUsers() {
+  const randomAvatar = () => {
+    const ranNum = Math.floor(Math.random() * 100);
+    return ranNum;
+  };
+  return (
+    <section className="flex flex-col gap-2 ">
+      <div>
+        <h2>Online Users:</h2>
+      </div>
+      <div className="flex gap-4">
+        <User name="Mahyar" description="online" className="" avatarProps={{ src: `https://i.pravatar.cc/${randomAvatar()}` }} />
+        <User name="John" description="online" avatarProps={{ src: `https://i.pravatar.cc/${randomAvatar()}` }} />
+        <User name="Jackson" description="online" avatarProps={{ src: `https://i.pravatar.cc/${randomAvatar()}` }} />
+      </div>
+    </section>
+  );
+}
+
+export default LiveUsers;
